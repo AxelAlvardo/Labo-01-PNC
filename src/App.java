c632acec5bd780816d4a4b5ccc0d4a5b0e6115d0
AxelAlvardo <00216022@uca.edu.sv>
2025-04-04 21:33:11 -0600

    Add : Interactive menu

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Menu menu = new Menu();
+
+        Match match = new Match(List.of("Jugador 1", "Jugador 2"), List.of("Jugador 3", "Jugador 4"), 0, 0);
         
+        menu.executeOption(match);
     }
 }
